e0cdc7e6cd251ad1361859de2bb3f7bbb9b0b047
Antonis Margaronis <antonymarg@gmail.com>
2023-06-29 17:21:23 +0300

    Persist state

--- src/Store/store.ts
+++ src/Store/store.ts
@@ -1,9 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { rootReducer } from '../Models';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['user'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
 });
 
 export type AppDispatch = typeof store.dispatch;
 export default store;
+export const persistor = persistStore(store);
