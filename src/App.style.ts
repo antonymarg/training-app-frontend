3d7398c12d25eb6861748b8dfa3f60b20ca9cee4
Antonis Margaronis <antonymarg@gmail.com>
2023-07-09 17:19:22 +0300

    Home page

--- src/App.style.ts
+++ src/App.style.ts
@@ -1,11 +1,15 @@
 import styled from 'styled-components';
 
 export const AppContainer = styled.div`
-  width: 100%;
+  width: 100%vw;
   height: 100%vh;
   background-color: ${(props) => props.theme.palette.background.default};
 `;
 
 export const LayoutContainer = styled.div`
-  margin: 0px 10%;
+  padding: 0px 10%;
+
+  @media only screen and (max-width: 768px) {
+    padding: 0px 16px;
+  }
 `;
--- src/Components/Navbar/navbar.style.ts
+++ src/Components/Navbar/navbar.style.ts
@@ -2,9 +2,13 @@ import styled from 'styled-components';
 
 export const StyledNav = styled.nav`
   background-color: ${(props) => props.theme.palette.primary.main};
-  padding: 10px 10%;
+  padding: 8px 10%;
   display: flex;
   flex-direction: row;
   min-height: 60px;
   align-items: center;
+
+  @media only screen and (max-width: 768px) {
+    padding: 8px 16px;
+  }
 `;
