6274d64dbb6fa7ff1a6fb2536ac580f6fff40491
Antonis Margaronis <antonis.margaronis@holidayextras.com>
2023-08-01 09:15:36 +0300

    assetModule

--- src/Firebase/index.ts
+++ src/Firebase/index.ts
@@ -1,6 +1,15 @@
 import { userModule } from './userModule/';
 import { trainingModule } from './trainingModule';
 import { notificationsModule } from './notificationsModule';
+import { assetsModule } from './assetsModule';
 import firebase, { auth, db } from './firebase';
 
-export { userModule, trainingModule, notificationsModule, firebase, db, auth };
+export {
+  userModule,
+  trainingModule,
+  notificationsModule,
+  assetsModule,
+  firebase,
+  db,
+  auth,
+};
